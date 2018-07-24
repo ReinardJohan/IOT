8aadc567ac2dfafb18afcfc887f3a8ed5e2704d0
ReinardJohan <astankill88@Gmail.com>
2018-07-24 22:47:21 +0700

    Add files via upload

---
+++ heartrate.R
@@ -0,0 +1,22 @@
+library(jsonlite)
+
+tableCols <- c("value")
+temp <- data.frame(matrix(ncol = length(tableCols), nrow = 0))
+colnames(temp) <- tableCols
+
+for (i in 1:10){
+  #source
+  jsonData = fromJSON("http://ihealth.sepdek.net/")
+  class(jsonData)
+  names(jsonData)
+  data <- jsonData$healthData
+  
+  #naming
+  filename <- format(Sys.time(),"%b_%d_%H_%M_%S_%Y")
+  myfile <- paste(filename,"_Heartrate",".csv")
+  
+  #filter
+  temp[i,] <- data[5, ]$value
+  Sys.sleep(2)
+}
+write.csv(temp, file = myfile)
